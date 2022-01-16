aa25661a7813be85ed476481ee15acf646a27a0f
Jøran Haines <joran.haines@gmail.com>
2022-01-16 15:10:49 +0530

    Add login and welcome fragments

---
+++ starter/app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.udacity.shoestore.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.udacity.shoestore.databinding.LoginFragmentBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: LoginFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = LoginFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+}
---
+++ starter/app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
@@ -0,0 +1,27 @@
+package com.udacity.shoestore.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        
+        val view = binding.root
+        return view
+    }
+
+}
